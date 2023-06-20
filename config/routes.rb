9ad3b1e6800dc5368ab3c0a1e7d9526025b2d68f
Yuuta1467 <s1341011we@gmail.com>
2023-06-20 19:36:34 +0000

    [route作成]

--- config/routes.rb
+++ config/routes.rb
@@ -7,29 +7,41 @@ Rails.application.routes.draw do
 
     scope module: :public do
       root to: "homes#top"
-      
+
       resources :post_images, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
         resource :favorites, only: [:create, :destroy]
         resources :post_comments, only: [:create, :edit, :update, :destroy] do
           resource :likes, only: [:create, :destroy]
-        end  
+        end
       end
-      
+
       resources :users, only: [:show, :edit, :update] do
         member do
           get :favorites
         end
       end
-      
+
       get 'users/:id/check' => 'users#check', as: 'check'
       patch 'users/:id/withdraw' => 'users#withdraw', as: 'withdraw'
-      
+
       post '/post_images/guest_sign_in', to: 'post#guest_sign_in'
-      
+
     end
-  
+
   devise_for :admin, skip: [:registrations, :passwords], controllers:{
     sessions: 'admin/sessions'
   }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    root to: "homes#top"
+
+    resources :post_images, only: [:index, :destroy] do
+      resources :post_comments, only: [:index, :show, :edit, :update, :destroy]
+    end
+
+    resources :tags, only: [:index, :create, :edit, :update, :destroy]
+
+    resources :users, only: [:index, :show, :edit, :update]
+
   end
+end
