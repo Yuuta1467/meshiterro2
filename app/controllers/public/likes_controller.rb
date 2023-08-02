49147b4bd96a785825786e02541f6d1b513d6a33
Yuuta1467 <s1341011we@gmail.com>
2023-08-02 00:11:09 +0000

    [コメントいいね途中]

--- app/controllers/public/likes_controller.rb
+++ app/controllers/public/likes_controller.rb
@@ -1,2 +1,22 @@
 class Public::LikesController < ApplicationController
+
+  def create
+    @post_image = PostImage.find(params[:post_image_id])
+    @post_comment = PostComment.find(params[:post_comment_id])
+    @like = Like.create(user_id: current_user.id, post_image_id: @post_image.id, post_comment_id: @post_comment.id)
+    #redirect_back(fallback_location: root_path)
+    #redirect_to post_path(@post_comment.post)
+    #非同期通信のためコメントアウト
+  end
+
+
+  def destroy
+    @post_image = PostImage.find(params[:post_image_id])
+    @post_comment = PostComment.find(params[:post_comment_id])
+    Like.find_by(user_id: current_user.id, post_image_id: @post_image.id, post_comment_id: @post_comment.id).destroy
+    #redirect_back(fallback_location: root_path)
+    #redirect_to post_path(@post_comment.post)
+    #非同期通信のためコメントアウト
+  end
+
 end
