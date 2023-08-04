8af4f6cfcf1f3afc63074827f8b45c361af002f7
Yuuta1467 <s1341011we@gmail.com>
2023-08-04 17:36:09 +0000

    [コメントいいね追加]

--- app/controllers/public/likes_controller.rb
+++ app/controllers/public/likes_controller.rb
@@ -4,7 +4,7 @@ class Public::LikesController < ApplicationController
     @post_image = PostImage.find(params[:post_image_id])
     @post_comment = PostComment.find(params[:post_comment_id])
     @like = Like.create(user_id: current_user.id, post_image_id: @post_image.id, post_comment_id: @post_comment.id)
-    #redirect_back(fallback_location: root_path)
+    redirect_back(fallback_location: root_path)
     #redirect_to post_path(@post_comment.post)
     #非同期通信のためコメントアウト
   end
@@ -14,7 +14,7 @@ class Public::LikesController < ApplicationController
     @post_image = PostImage.find(params[:post_image_id])
     @post_comment = PostComment.find(params[:post_comment_id])
     Like.find_by(user_id: current_user.id, post_image_id: @post_image.id, post_comment_id: @post_comment.id).destroy
-    #redirect_back(fallback_location: root_path)
+    redirect_back(fallback_location: root_path)
     #redirect_to post_path(@post_comment.post)
     #非同期通信のためコメントアウト
   end
