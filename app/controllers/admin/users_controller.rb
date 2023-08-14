0a5bb204a86362e2d52e0413ffb600e9c7ec3d03
Yuuta1467 <s1341011we@gmail.com>
2023-08-14 16:28:34 +0000

    [user一覧]

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,8 @@
 class Admin::UsersController < ApplicationController
+    before_action :authenticate_admin!
   def index
+    @users = User.page(params[:page]).order(created_at: :desc)
+    #byebug
   end
 
   def show
